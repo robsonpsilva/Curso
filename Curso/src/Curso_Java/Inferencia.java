1c1bfc1737fe7b698547b3e04920cccb0ec803b3
Robson <robson_p_s@yahoo.com.br>
2020-07-21 01:24:21 +0000

    Salve inferencia

---
+++ Curso/src/Curso_Java/Inferencia.java
@@ -0,0 +1,11 @@
+package Curso_Java;
+
+public class Inferencia {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		var b = 4.5;
+		System.out.println(b);
+	}
+
+}
